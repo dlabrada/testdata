afb776f934aa4b38f7bcb5695736d1e72839b61b
Diego Labrada <dlabrada.rodriguez@gmail.com>
2022-12-18 11:01:20 -0300

    probe BD

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'dlabrada',
+  password : 'D26y12an@',
+  database : 'cloudtek'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM user', function (error, results, fields) {
+  try {
+    if (error) throw error;
+    console.log('The solution is: ', results[0]);
+    
+  } catch (error) {
+    console.log(error)
+  }
+ 
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
